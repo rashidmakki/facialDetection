a76717eba35f755121d597a8e3b61acd37e20315
Rashid Makki <rashidmakkiha_cse18@its.edu.in>
2020-10-20 18:29:33 +0530

    Updates

---
+++ precache-manifest.11cfdafd445917e863e707df89afb20c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b243ecab5550252189d6b9f430401c",
+    "url": "/facialDetection/index.html"
+  },
+  {
+    "revision": "efc732fedf5a371bd24a",
+    "url": "/facialDetection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba4e389b783d1824e298",
+    "url": "/facialDetection/static/js/2.baa0a5cb.chunk.js"
+  },
+  {
+    "revision": "6a3f217cdca97ce53e338d179fb23279",
+    "url": "/facialDetection/static/js/2.baa0a5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc732fedf5a371bd24a",
+    "url": "/facialDetection/static/js/main.19ed4b52.chunk.js"
+  },
+  {
+    "revision": "8e395b103d71e457bcdf",
+    "url": "/facialDetection/static/js/runtime-main.2e678be4.js"
+  }
+]);
